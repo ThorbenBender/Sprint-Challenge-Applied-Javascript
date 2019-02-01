af33828d57840c197760c1ab0dbfd6330de2f1ab
Thorben Bender <bender.thorben0@gmail.com>
2019-02-01 10:21:41 +0100

    changed the tablink class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,8 +13,9 @@ class TabLink {
       this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`.card[data-tab="${this.tabData}"]`);
+      this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
     }
+    console.log(this);
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class.
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
@@ -26,19 +27,18 @@ class TabLink {
 
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
-    
+    console.log(tabs);
     // Iterate through the NodeList removing the .active-tab class from each element
     tabs.forEach(tab => tab.classList.remove('active-tab'));
 
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll('.card');
-
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach(card => card.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
     this.tabElement.classList.add('active-tab');
-  
+    console.log(this, 'this should return this.cards');
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     this.cards.forEach(card => card.selectCard());
   }
